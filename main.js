31e81f0582afdb91f0f43adfa0ffc6f51d279b55
Steven Lundy <steven.lundy@gmail.com>
2015-11-18 17:59:51 -0800

    Refactor getLineNumber to take charIndex

--- main.js
+++ main.js
@@ -5,9 +5,13 @@ $(document).on('ready', function() {
     registerKeyPress(e, this);
   };
   registerKeyPress = function(e, el) {
-    if(map[16] && map[17] && map[68]) {
+    if(map[16] && map[17] && map[68]) { //ctrl+shift+D
       e.preventDefault();
       duplicateLine(el);
+    }
+    if(map[16] && map[17] && map[]) { //ctrl+shift+up
+      e.preventDefault();
+      shiftLinesUp(el);
     }
   };
   $('textarea').on('keydown', setState);
@@ -23,7 +27,7 @@ $(document).on('ready', function() {
 var duplicateLine = function(el) {
   var lines = el.value.split(String.fromCharCode(10));
   var start = el.selectionStart;
-  var lineNumber = getLineNumber(el);
+  var lineNumber = getLineNumberAtIndex(el, start);
   lines.splice(lineNumber, 0, lines[lineNumber]);
   el.value = lines.join(String.fromCharCode(10));
   el.selectionStart = start + lines[lineNumber].length + 1;
@@ -33,16 +37,16 @@ var duplicateLine = function(el) {
 
 var getLine = function(el) {
   var lines = el.value.split(String.fromCharCode(10));
-  var lineNumber = getLineNumber(el);
+  var lineNumber = getLineNumberAtIndex(el, el.selectionStart);
   return line[lineNumber];
 };
 
-var getLineNumber = function(el) {
+var getLineNumberAtIndex = function(el, index) {
   var lines = el.value.split(String.fromCharCode(10));
   var charNumber = 0;
   for(var i = 0; i < lines.length; i++) {
     charNumber += lines[i].length + 1;
-    if (charNumber > el.selectionStart) {
+    if (charNumber > index) {
       return i;
     }
   }
